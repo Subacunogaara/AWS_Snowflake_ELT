73b8751488816ed6c4973a757d9c3ef9bf1e0fb9
Subacunogaara <75877293+Subacunogaara@users.noreply.github.com>
2023-08-29 00:02:05 +0200

    Update snowflake_worksheet.sql

--- snowflake_worksheet.sql
+++ snowflake_worksheet.sql
@@ -30,22 +30,24 @@ CREATE OR REPLACE stage stg_json_folder
     STORAGE_INTEGRATION = s3_integr
     FILE_FORMAT = json_fileformat;
 
+//Создаем и заполняем временную таблицу данными из S3
+CREATE OR REPLACE TEMPORARY TABLE tmp_s3_table AS
+SELECT
+    $1:event_data:data:eventData:app_user_id AS player_id,
+    $1:event_data:data:eventData:platformAccountId AS device_id,
+    $1:event_data:timestampClient::datetime AS install_date,
+    $1:event_data:platform AS client_id,
+    $1:event_data:appName AS app_name,
+    $1:event_data:countryCode AS country
+FROM @stg_json_folder
+WHERE $1:event_data:data:eventData:eventType = 'server_install';
 
-//Заполняем таблицу данными из S3
-COPY INTO raw_table 
-FROM(
-    SELECT
-        $1:event_data:data:eventData:app_user_id AS player_id,
-        $1:event_data:data:eventData:platformAccountId AS device_id,
-        $1:event_data:timestampClient::datetime AS install_date,
-        $1:event_data:platform AS client_id,
-        $1:event_data:appName AS app_name,
-        $1:event_data:countryCode AS country
-    FROM @stg_json_folder
-    WHERE $1:event_data:data:eventData:eventType = 'server_install'
-    );
+//Заполняем ранее созданную таблицу
+INSERT INTO raw_table
+SELECT * FROM tmp_s3_table;
 
-
+//Удаляем временную таблицу
+DROP TABLE IF EXISTS tmp_s3_table;
 
 
 
