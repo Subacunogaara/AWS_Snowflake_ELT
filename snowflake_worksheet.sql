0e1535c5027d8b4f6ce5321f5ff4ac88c17f22c0
Subacunogaara <75877293+Subacunogaara@users.noreply.github.com>
2023-08-28 23:26:24 +0200

    Update snowflake_worksheet.sql

--- snowflake_worksheet.sql
+++ snowflake_worksheet.sql
@@ -28,16 +28,36 @@ CREATE OR REPLACE file format json_fileformat
 CREATE OR REPLACE stage stg_json_folder
     URL = 's3://snowflakeelts3bucket/json_folder'
     STORAGE_INTEGRATION = s3_integr
-    FILE_FORMAT = json_fileformat
+    FILE_FORMAT = json_fileformat;
+
 
 //Заполняем таблицу данными из S3
-COPY INTO raw_table
-SELECT
-    $1:event_data:data:eventData:app_user_id AS player_id,
-    $1:event_data:data:eventData:platformAccountId AS device_id,
-    $1:event_data:timestampClient::datetime AS install_date,
-    $1:event_data:platform AS client_id,
-    $1:event_data:appName AS app_name,
-    $1:event_data:countryCode AS country
-FROM @stg_json_folder
-WHERE $1:event_data:data:eventData:eventType = 'server_install';
+COPY INTO raw_table 
+FROM(
+    SELECT
+        $1:event_data:data:eventData:app_user_id AS player_id,
+        $1:event_data:data:eventData:platformAccountId AS device_id,
+        $1:event_data:timestampClient::datetime AS install_date,
+        $1:event_data:platform AS client_id,
+        $1:event_data:appName AS app_name,
+        $1:event_data:countryCode AS country
+    FROM @stg_json_folder
+    WHERE $1:event_data:data:eventData:eventType = 'server_install'
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
