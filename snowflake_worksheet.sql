bd9fc31641a7672a312248845ef1c09c423a1f4b
Subacunogaara <75877293+Subacunogaara@users.noreply.github.com>
2023-08-29 18:13:52 +0200

    Update snowflake_worksheet.sql

--- snowflake_worksheet.sql
+++ snowflake_worksheet.sql
@@ -1,4 +1,4 @@
-// Создание обьекта storage integration для AWS S3
+    // Creating storage integration for AWS S3
 CREATE OR REPLACE STORAGE INTEGRATION s3_integr
     TYPE = EXTERNAL_STAGE
     STORAGE_PROVIDER = S3
@@ -6,32 +6,60 @@ CREATE OR REPLACE STORAGE INTEGRATION s3_integr
     STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::083226907131:role/snowflake-access-role'
     STORAGE_ALLOWED_LOCATIONS = ('s3://snowflakeelts3bucket/json_folder');
 
-//Для настройки policy
+    //For setting policy
 DESC INTEGRATION s3_integr;
 
-//Создание новой схемы и таблицы для данных из S3
+    //Creating a new schema for data from S3
 CREATE OR REPLACE SCHEMA S3;
 
-CREATE OR REPLACE TABLE raw_table (
-    player_id STRING,
-    device_id STRING,
-    install_date DATETIME,
-    client_id STRING,
-    app_name STRING,
-    country STRING);
-
-// Создание обьекта file format для json
+    //Creating file format for json files
 CREATE OR REPLACE file format json_fileformat
     TYPE = json
 
-//Создание объекта stage 
+    
+    //Creating stage object for json files
+    //with integration object and file format
 CREATE OR REPLACE stage stg_json_folder
-    URL = 's3://snowflakeelts3bucket/json_folder'
+    URL = 's3://snowflakeelts3bucket/json_folder/snowpipe'
     STORAGE_INTEGRATION = s3_integr
     FILE_FORMAT = json_fileformat;
 
-//Создаем и заполняем временную таблицу данными из S3
-CREATE OR REPLACE TEMPORARY TABLE tmp_s3_table AS
+    //For checking the information inside the bucket
+LIST @stg_json_folder;
+
+
+    //Creating a table for raw data
+CREATE OR REPLACE TABLE raw_data_table(
+raw_data VARIANT);
+
+
+    //Creating pipe object
+CREATE OR REPLACE PIPE json_pipe
+auto_ingest = TRUE
+AS
+COPY INTO raw_data_table
+FROM @stg_json_folder;
+
+    //For sitting up notiffications
+DESC PIPE json_pipe;
+
+
+    //Creating the final table for clean data
+CREATE OR REPLACE TABLE server_install_table(
+player_id VARCHAR,
+device_id VARCHAR,
+install_date DATETIME,
+client_id VARCHAR,
+app_name VARCHAR,
+country VARCHAR
+);
+
+    //Creating stream for raw data table
+CREATE OR REPLACE STREAM json_stream ON TABLE raw_data_table;
+
+
+    
+INSERT INTO server_install_table(
 SELECT
     $1:event_data:data:eventData:app_user_id AS player_id,
     $1:event_data:data:eventData:platformAccountId AS device_id,
@@ -39,27 +67,6 @@ SELECT
     $1:event_data:platform AS client_id,
     $1:event_data:appName AS app_name,
     $1:event_data:countryCode AS country
-FROM @stg_json_folder
-WHERE $1:event_data:data:eventData:eventType = 'server_install';
-
-//Заполняем ранее созданную таблицу
-INSERT INTO raw_table
-SELECT * FROM tmp_s3_table;
-
-//Удаляем временную таблицу
-DROP TABLE IF EXISTS tmp_s3_table;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+FROM json_stream
+WHERE $1:event_data:data:eventData:eventType = 'server_install'
+);
